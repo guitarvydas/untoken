7a4e78b230e95ecd3548729c188c448cda363566
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-16 11:28:58 -0500

    user ../untoken/untoken.awk

--- untoken.bash
+++ untoken.bash
@@ -1,4 +1,2 @@
 #!/bin/bash
-# sed -e 's/\[\([[:alphanum:]]+\) \([[:alphanum:]]+\) \([[:digit:]]+\) \([[:digit:]]+\)\]/\2/g'
-#sed -e 's/\[\([%0-9A-Za-z]+\) +\([[%0-9A-Za-z]]+\) +\([0-9]+\) +\([0-9]+\)\]/\2/g'
-sed -e 's/[a-z]*/x/g'
+awk -f ../untoken/untoken.awk
